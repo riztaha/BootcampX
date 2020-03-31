11e132b501e30b1bdf10fd6676e44917f362feec
Taha Rizvi <riztaha@gmail.com>
2020-03-31 22:52:31 +0000

    Query to list average assistance time by cohort

--- 4_queries/avg_assistance_time.sql
+++ 4_queries/avg_assistance_time.sql
@@ -1,17 +1,6 @@
-
--- SELECT ((SELECT (completed_at-started_at) as duration
--- FROM assistance_requests
--- JOIN teachers ON teachers.id = teacher_id
--- JOIN students ON students.id = student_id
--- JOIN assignments ON assignments.id = assignment_id
--- WHERE teachers.name = 'Helmer Rodriguez' AND students.name = 'Maximillian Pfannerstill'
--- ORDER BY duration) - (SELECT (completed_at-started_at) as duration
--- FROM assistance_requests
--- JOIN teachers ON teachers.id = teacher_id
--- JOIN students ON students.id = student_id
--- JOIN assignments ON assignments.id = assignment_id
--- WHERE teachers.name like 'Rosalyn%' AND students.name like 'Wallace%' AND assignments.name = 'Cupiditate explicabo repellendus'
--- ORDER BY duration)) as average_assistance_request_duration
-
-SELECT avg(completed_at - started_at) as average_assistance_request_duration
-FROM assistance_requests;
+SELECT cohorts.name as name, avg(completed_at - started_at) as average_assistance_time
+FROM assistance_requests
+JOIN students ON students.id = student_id
+JOIN cohorts ON cohort_id = cohorts.id
+GROUP BY cohorts.name
+ORDER BY average_assistance_time;
